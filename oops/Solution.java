31caaa6964ede8e980eb8b2c656d9523aa22c350
Abhishek Kumar <guptabhi49@gmail.com>
2023-06-08 21:01:40 +0530

    pushing new file

---
+++ oops/Solution.java
@@ -0,0 +1,12 @@
+package oops;
+public class Solution {
+    public static void main(String[] args) {
+        student st = new student();
+        st.studentName = "Raja";
+        st.studentId= 2456;
+
+        st.DisplayDetails();
+
+        
+    }
+}
---
+++ oops/student.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class student {
+    String studentName;
+    int studentId;
+
+    void DisplayDetails(){
+        System.out.println("My Name is "+studentName);
+        System.out.println("My ID is "+studentId);
+    }
+}
